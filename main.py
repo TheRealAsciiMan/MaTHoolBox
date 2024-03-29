550ad55c3ae74c594e440905a87b775fb5e6aa2c
TheRealAsciiMan <147166455+TheRealAsciiMan@users.noreply.github.com>
2024-03-29 15:25:37 +0100

    Menu en cours

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def integrale(a=0, b=5, fonc=lambda x: x ** 3, n=15):
     @params :
     - fonc : fonction python
     - a : 1ère borne de l'intervalle
-    - h : 2ème borne de l'intervalle
+    - b : 2ème borne de l'intervalle
     - n : Nombre de trapèzes
     @returns :
     - Une valeur approchée de l'intégrale
@@ -105,3 +105,64 @@ def integrale(a=0, b=5, fonc=lambda x: x ** 3, n=15):
         d = g + dist
         surf += (dist * (fonc(d)+fonc(g) )) / 2
     return surf
+
+def integrale_rec(a=0.0, b=5.0, fonc=lambda x: x ** 3, p=1e-6):
+    """Calcul l'intégrale d'une fonction entre le point a et le point b de façon récursive
+    @params :
+    - fonc : fonction python
+    - a : 1ère borne de l'intervalle
+    - h : 2ème borne de l'intervalle
+    - p : Précision voulue
+    @returns :
+    - Une valeur approchée de l'intégrale
+    """
+    if a > b: a, b = b, a
+    if b-a < p :
+        return (b-a)*fonc((b+a)/2)
+    else:
+        return integrale_rec(a, b-((b-a)/2), fonc, p) + integrale_rec(a+((b-a)/2), b, fonc, p)
+
+
+print("""Bienvenue sur MaTHoolBox !
+Un programme qui permet de résoudre certains problèmes de Mathématiques Appliquées
+""")
+
+def menu():
+    print("""
+    Veuillez Choisir l'outil à utiliser :
+    1. Résolution d'équation de degré 2
+    2. Résolution d'équation de degré n par dichotomie
+    3. Calcule la dérivée d'une fonction en un point
+    4. Calcule l'intégral d'une fonction d'un point a à un point b
+    5. Quitter
+    
+    """)
+
+def degre_menu():
+    while True:
+        print("""Résolution d'équation de la forme ax²+bx+c=0, veuillez choisir les facteurs :
+        """)
+        try:
+            a = float(input("a : "))
+            b = float(input("b : "))
+            c = float(input("c : "))
+            break
+        except:
+            print("Ce ne sont pas des nombres, veuillez réessayer !")
+
+
+
+
+
+while True:
+    menu()
+    reponse = input()
+    if reponse == "1":
+        degre_menu()
+    if reponse == "2":
+    if reponse == "3":
+    if reponse == "4":
+    if reponse == "5":
+        break
+    else:
+        print("Erreur, veuillez choisir une option du menu")
